0674ee9476dc8a47f56ab68492d83393b1c6f590
Sam Maisha <sam.maisha99@gmail.com>
2023-05-01 18:28:15 +0000

    add data into employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,24 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+### Exercise 6: One-to-many association
+
+# We haven't used the Employee class (and employees table) at all yet. If you look at this table's column structure, you'll find that it has a `store_id` (integer) column. This is a column that identifies which store each employee belongs to. It points to the `id` (integer) column of their store.
+
+# Let's tell Active Record that these two tables are in fact related via the `store_id` column.
+
+# 1. Add the following code _directly_ inside the Store model (class): `has_many :employees`
+# 2. Add the following code directly inside the Employee model (class): `belongs_to :store`
+# 3. Add some data into employees. Here's an example of one (note how it differs from how you create stores): `@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)`
+# 4. Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the `@store#` instance variables that you defined in previous exercises. Create a bunch under `@store1` (Burnaby) and `@store2` (Richmond). Eg: `@store1.employees.create(...)`.
+@store4 = Store.find_by(id: 4)
+@store5 = Store.find_by(id: 5)
+@store6 = Store.find_by(id: 6)
+
+@store1.employees.create(first_name: "Sam", last_name: "Maisha", hourly_rate: 100)
+@store1.employees.create(first_name: "Cody", last_name: "Hilborn", hourly_rate: 100)
+@store2.employees.create(first_name: "Zen", last_name: "Taskin", hourly_rate: 90)
+@store4.employees.create(first_name: "Ashab", last_name: "Chowdhury", hourly_rate: 60)
+@store5.employees.create(first_name: "Randy", last_name: "Chung", hourly_rate: 120)
+@store6.employees.create(first_name: "Neil", last_name: "Sherman", hourly_rate: 120)
+
